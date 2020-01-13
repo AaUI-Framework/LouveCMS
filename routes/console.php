e34765696bc2d617bb652e70d1ce3c520f3da92a
alessandroamos <alessandroamos@icloud.com>
2020-01-13 14:17:24 -0300

    Console command for create user and admin

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,39 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+
+//COMMAND FOR CREATE USER
+Artisan::command('create:user', function () {
+
+    $name = $this->ask('Name?');
+    $email = $this->ask('Email?');
+    $pwd = $this->ask('Password?');
+    // $pwd = $this->secret('Password?'); // or use secret() to hide the password being inputted
+    \DB::table('users')->insert([
+        'name' => $name,
+        'email' => $email,
+        'password' => bcrypt($pwd),
+        'created_at' => date_create()->format('Y-m-d H:i:s'),
+        'updated_at' => date_create()->format('Y-m-d H:i:s'),
+    ]);
+    $this->info('Account created for '.$name);
+});
+
+//COMMAND FOR CREATE ADMIN
+Artisan::command('create:admin', function () {
+
+    $name = $this->ask('Name?');
+    $email = $this->ask('Email?');
+    $pwd = $this->ask('Password?');
+    // $pwd = $this->secret('Password?'); // or use secret() to hide the password being inputted
+    \DB::table('admins')->insert([
+        'name' => $name,
+        'email' => $email,
+        'password' => bcrypt($pwd),
+        'created_at' => date_create()->format('Y-m-d H:i:s'),
+        'updated_at' => date_create()->format('Y-m-d H:i:s'),
+    ]);
+    $this->info('Account created for '.$name);
+});
+
